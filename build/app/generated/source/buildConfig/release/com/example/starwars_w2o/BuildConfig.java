a3bedf01405c4159b5b7d06b24f629967536b3c0
Leonardo <leo.he.martins@gmail.com>
2022-10-20 15:00:50 -0300

    first commit

---
+++ build/app/generated/source/buildConfig/release/com/example/starwars_w2o/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.starwars_w2o;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.starwars_w2o";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
